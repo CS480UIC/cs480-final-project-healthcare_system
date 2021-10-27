d07422126c408d648a448fbbbe23a55d2e68f5dd
karan9617 <karan.vish17@gmail.com>
2021-10-26 22:36:45 -0500

    employee-hospital entity

--- src/assignment6/index.js
+++ src/assignment6/index.js
@@ -14,7 +14,52 @@ app.get('/', (req, res) => {
     res.render("index", { response: { name: "" } });
 
 })
+// Employee routes
+app.get('/employee/employee', (req, res) => {
+    res.render("employee/employee", { response: { name: "" } });
 
+})
+app.get('/employee/create', (req, res) => {
+    res.render("employee/create", { response: { name: "" } });
+
+})
+app.get('/employee/update', (req, res) => {
+    res.render("employee/update", { response: { name: "" } });
+
+})
+app.get('/employee/delete', (req, res) => {
+    res.render("employee/delete", { response: { name: "" } });
+
+})
+app.get('/employee/read', (req, res) => {
+    res.render("employee/read", { response: { name: "" } });
+
+})
+// Hospital routes
+app.get('/hospital/hospital', (req, res) => {
+    res.render("hospital/hospital", { response: { name: "" } });
+
+})
+app.get('/hospital/create', (req, res) => {
+    res.render("hospital/create", { response: { name: "" } });
+
+})
+app.get('/hospital/update', (req, res) => {
+    res.render("hospital/update", { response: { name: "" } });
+
+})
+app.get('/hospital/delete', (req, res) => {
+    res.render("hospital/delete", { response: { name: "" } });
+
+})
+app.get('/hospital/read', (req, res) => {
+    res.render("hospital/read", { response: { name: "" } });
+
+})
+
+
+
+// connecing to localhost
 app.listen(3000, () => {
     console.log('server started port  3000');
 })
@@ -26,4 +71,5 @@ const db = mysql.createConnection({
     password: 'password',
     database: 'healthcare_system'
 });
+
 
