648de007aa449337886df5f1a2adbde1b3e38452
karan9617 <karan.vish17@gmail.com>
2021-11-09 17:21:01 -0600

    delete.sql

---
+++ sql/deleteDB.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `employee`;
+
+DROP TABLE IF EXISTS `doctor`;
+
+DROP TABLE IF EXISTS `hospital`;
+
+DROP TABLE IF EXISTS `medicine`;
+
+DROP TABLE IF EXISTS `mode_of_payment`;
+
+DROP TABLE IF EXISTS `patient`;
+
+DROP TABLE IF EXISTS `patient_feedback`;
